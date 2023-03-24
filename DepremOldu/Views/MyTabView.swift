5a31c966a79b46aa811b9d7d180d4b7e7643c90b
U200709077 <ozancck@icloud.com>
2023-03-24 15:27:51 +0300

    Update MyTabView.swift

--- DepremOldu/Views/MyTabView.swift
+++ DepremOldu/Views/MyTabView.swift
@@ -14,15 +14,22 @@ struct MyTabView: View {
                 .tabItem {
                     Label("Tüm Depremler", systemImage: "waveform.path.ecg.rectangle.fill")
                 }
-                
 
             FilterEarthquakeView()
                 .tabItem {
                     Label("Büyük Depremler", systemImage: "alarm.waves.left.and.right.fill")
                 }
-            
         }
         .tableStyle(.inset)
+        .onAppear {
+            let tabBarAppearance = UITabBarAppearance()
+            tabBarAppearance.configureWithOpaqueBackground()
+            UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithOpaqueBackground()
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        }
     }
 }
 
